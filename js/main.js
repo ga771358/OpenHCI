75fa06b3221cb3d9abd65a81b4c853239c77762c
ktyang86267 <aroe86267@gmail.com>
2017-05-10 17:00:41 +0800

    first update

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  // scroll to element
+    $('nav a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 900, 'swing');
+    });
+});
+
+$(document).ready(function(){
+  // scroll to element
+    $('.container a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 900, 'swing');
+    });
+});
