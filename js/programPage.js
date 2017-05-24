26d4adaef2417faec651a07b86ddaad3927e897f
ric113 <ric1134991@gmail.com>
2017-05-24 22:10:16 +0800

    Program Page update , but RWD still not okay

--- js/programPage.js
+++ js/programPage.js
@@ -1,21 +1,22 @@
 (()=>{
 	$(document).ready( () => {
 
-		const upArrow = document.querySelector("#program .left-side-container .btn-up");
-		const downArrow = document.querySelector("#program .left-side-container .btn-down");
+		const backBtn = document.querySelector("#program .left-side-bar .back-btn");
+		const nextBtn = document.querySelector("#program .left-side-bar .next-btn");
+
 		const textTimeLine = new TimelineMax();
 		const dateTimeLine = new TimelineMax();
-		const speakerTimeLine = new TimelineMax();
+		const titleTimeLine = new TimelineMax();
 
-		const activityDay = 6;
-		let currentDay = 1;
+		const pages = 2;
+		let currentPage = 1;
 		let animationComplete = true;
 
-		const diableArrowHandler = function(){
+		const diableBtnHandler = function(){
 			animationComplete = false;
 		}
 
-		const enableArrowHandler = function(){
+		const enableBtnHandler = function(){
 			animationComplete = true;
 		}
 
@@ -26,114 +27,140 @@
 			windowHeight < containerHeight && $(`#program`).css("min-height", `${containerHeight}px`);
 		}
 
-		const upArrowHandler = function(e){
-			currentDay --;
-			if(currentDay > 0 && animationComplete) {
-				console.log(currentDay);
-
-				
-				updateContainerHeight();
-				diableArrowHandler();
-
-				// Update Content .
-				const preDayScheduleText = document.querySelectorAll(`#program .day${currentDay+1}-content p`);
-				const preDayDateText = document.querySelector(`#program .day${currentDay+1}-content .date`);
-				const preDayTitleText = document.querySelector(`#program .day${currentDay+1}-content .day`);
-				const currentDayScheduleText = document.querySelectorAll(`#program .day${currentDay}-content p`);
-				const currentDayDateText = document.querySelector(`#program .day${currentDay}-content .date`);
-				const currentDayTitleText = document.querySelector(`#program .day${currentDay}-content .day`);
-
-	 
-				textTimeLine.staggerFromTo([preDayTitleText,...preDayScheduleText], 0.5,{opacity:1,y:0},{ opacity:0,y:-30,ease:Power4.easeIn},0.03)
-				   .staggerFromTo([currentDayTitleText,...currentDayScheduleText], 0.5, {opacity:0, y:30}, {opacity:1,y:0,ease: Power4.easeIn},0.03);
-
-				dateTimeLine.fromTo(preDayDateText, 0.5,{opacity:1,x:0},{ opacity:0,x:-30,ease:Power4.easeIn})
-				   .fromTo(currentDayDateText, 0.5, {opacity:0, x:30}, {opacity:1,x:0,ease: Power4.easeIn, onComplete:enableArrowHandler});
+		const backBtnHandler = function(e){
+			console.log("Back");
 			
-				// Update Speaker .
-				const preSpeaker = document.querySelector(`#program .day${currentDay+1}-speaker`);
-				const currentSpeaker = document.querySelector(`#program .day${currentDay}-speaker`);
-				let fadeInDelay = 0 ;
+			currentPage --;
+			
+			if(currentPage > 0 && animationComplete) {
+				console.log(currentPage);
+
 				
-				if(preSpeaker !== null)
-					speakerTimeLine.fromTo(preSpeaker,0.5,{opacity:1},{opacity:0});
-				else
-					fadeInDelay = 0.5;
-				if(currentSpeaker !== null)
-					speakerTimeLine.fromTo(currentSpeaker,1.2,{opacity:0},{opacity:1,ease: Power4.easeIn,delay:fadeInDelay});
-				
-				// Update Button Text .
-				const preBtnText =  (currentDay-1 < 1) ? "" : `Day ${currentDay-1}`;
-				const nextBtnText =  (currentDay+1 > activityDay) ? "" : `Day ${currentDay+1}`;
-
-				$("#program .btn-up .text").fadeOut(800, function() {
-        			$(this).text(preBtnText).fadeIn(800);
-        		});
-
-        		$("#program .btn-down .text").fadeOut(800, function() {
-        			$(this).text(nextBtnText).fadeIn(800);
-        		});
-				
-			} else {
-				currentDay ++ ;
-			}
-		}
-
-		const downArrowHandler = function(e){
-			currentDay ++;
-			if(currentDay <= activityDay && animationComplete){
-				console.log(currentDay);
-
-				updateContainerHeight();
-				diableArrowHandler();
+				// updateContainerHeight();
+				diableBtnHandler();
 
 				// Update Content .
-				const preDayScheduleText = document.querySelectorAll(`#program .day${currentDay-1}-content p`);
-				const preDayDateText = document.querySelector(`#program .day${currentDay-1}-content .date`);
-				const preDayTitleText = document.querySelector(`#program .day${currentDay-1}-content .day`);
-				const currentDayScheduleText = document.querySelectorAll(`#program .day${currentDay}-content p`);
-				const currentDayDateText = document.querySelector(`#program .day${currentDay}-content .date`);
-				const currentDayTitleText = document.querySelector(`#program .day${currentDay}-content .day`);
-
-				textTimeLine.staggerFromTo([preDayTitleText,...preDayScheduleText], 0.5,{opacity:1,y:0},{ opacity:0,y:-30,ease:Power4.easeIn},0.03)
-				  .staggerFromTo([currentDayTitleText,...currentDayScheduleText], 0.5, {opacity:0, y:30}, {opacity:1,y:0,ease: Power4.easeIn},0.03);
-
-				dateTimeLine.fromTo(preDayDateText, 0.5,{opacity:1,x:0},{ opacity:0,x:-30,ease:Power4.easeIn})
-				   .fromTo(currentDayDateText, 0.5, {opacity:0, x:30}, {opacity:1,x:0,ease: Power4.easeIn, onComplete:enableArrowHandler});
 				
+				let currentDateImgs = [];
+				let currentTitles = [];
+				let currentIcons = [];
+				let currentScheduleTexts = [];
 
-				// Update Speaker .
-				const preSpeaker = document.querySelector(`#program .day${currentDay-1}-speaker`);
-				const currentSpeaker = document.querySelector(`#program .day${currentDay}-speaker`);
-				let fadeInDelay = 0;
-				
-				if(preSpeaker !== null)
-					speakerTimeLine.fromTo(preSpeaker,0.5,{opacity:1},{opacity:0});
-				else
-					fadeInDelay = 0.5;
-				
-				if(currentSpeaker !== null)
-					speakerTimeLine.fromTo(currentSpeaker,1.2,{opacity:0},{opacity:1,ease: Power4.easeIn,delay:fadeInDelay});
+				for(let i = 0 ; i < 3 ; i ++){
+					currentDateImgs.push(document.querySelector(`#program .day-block${i+1} .page${currentPage} .date`));
+					currentTitles.push(document.querySelector(`#program .day-block${i+1} .page${currentPage} .title`));
+					currentIcons.push(document.querySelector(`#program .day-block${i+1} .page${currentPage} .icon`));
+					currentScheduleTexts.push(document.querySelectorAll(`#program .day-block${i+1} .page${currentPage} p`));
+				}
 
-				// Update Button Text .
-				const preBtnText =  (currentDay-1 < 1) ? "" : `Day ${currentDay-1}`;
-				const nextBtnText =  (currentDay+1 > activityDay) ? "" : `Day ${currentDay+1}`;
+				let preDateImgs = [];
+				let preTitles = [];
+				let preIcons = [];
+				let preScheduleTexts = [];
 
-				$("#program .btn-up .text").fadeOut(800, function() {
-        			$(this).text(preBtnText).fadeIn(800);
-        		});
+				for(let i = 0 ; i < 3 ; i ++){
+					preDateImgs.push(document.querySelector(`#program .day-block${i+1} .page${currentPage+1} .date`));
+					preTitles.push(document.querySelector(`#program .day-block${i+1} .page${currentPage+1} .title`));
+					preIcons.push(document.querySelector(`#program .day-block${i+1} .page${currentPage+1} .icon`));
+					preScheduleTexts.push(document.querySelectorAll(`#program .day-block${i+1} .page${currentPage+1} p`));
+				}
 
-        		$("#program .btn-down .text").fadeOut(800, function() {
-        			$(this).text(nextBtnText).fadeIn(800);
-        		});
+				for(let i = 0 ; i < 3 ; i ++){
+
+					let dateImgsTimeLine = new TimelineMax();
+					let titlesTimeLine = new TimelineMax();
+					let iconsTimeLine = new TimelineMax();
+					let scheduleTextsTimeLine = new TimelineMax();
+
+					dateImgsTimeLine
+						.fromTo(preDateImgs[i],0.5,{opacity:1},{opacity:0})
+						.fromTo(currentDateImgs[i],0.5,{opacity:0},{opacity:1});
+					titlesTimeLine
+						.fromTo(preTitles[i], 0.5,{opacity:1,x:0},{ opacity:0,x:-30,ease:Power4.easeIn})
+				   		.fromTo(currentTitles[i], 0.5, {opacity:0, x:30}, {opacity:1,x:0,ease: Power4.easeIn});
+					iconsTimeLine
+						.fromTo(preIcons[i], 0.5,{opacity:1,x:0},{ opacity:0,x:-30,ease:Power4.easeIn})
+				   		.fromTo(currentIcons[i], 0.5, {opacity:0, x:30}, {opacity:1,x:0,ease: Power4.easeIn, onComplete:enableBtnHandler});
+					scheduleTextsTimeLine
+						.staggerFromTo(preScheduleTexts[i], 0.5,{opacity:1,y:0},{ opacity:0,y:-30,ease:Power4.easeIn},0.03)
+				   		.staggerFromTo(currentScheduleTexts[i], 0.5, {opacity:0, y:30}, {opacity:1,y:0,ease: Power4.easeIn},0.03);
+				}
 				
 			} else {
-				currentDay --;
-			}	
+				currentPage ++ ;
+			}
+			
 		}
 
-		upArrow.addEventListener("click",upArrowHandler);
-		downArrow.addEventListener("click",downArrowHandler);
+		const nextBtnHandler = function(e){
+			console.log("next");
+			
+			currentPage ++;
+			
+			if(currentPage <= pages && animationComplete){
+				console.log(currentPage);
+
+				
+				// updateContainerHeight();
+				diableBtnHandler();
+
+				// Update Content .
+				
+
+				let currentDateImgs = [];
+				let currentTitles = [];
+				let currentIcons = [];
+				let currentScheduleTexts = [];
+
+				for(let i = 0 ; i < 3 ; i ++){
+					currentDateImgs.push(document.querySelector(`#program .day-block${i+1} .page${currentPage} .date`));
+					currentTitles.push(document.querySelector(`#program .day-block${i+1} .page${currentPage} .title`));
+					currentIcons.push(document.querySelector(`#program .day-block${i+1} .page${currentPage} .icon`));
+					currentScheduleTexts.push(document.querySelectorAll(`#program .day-block${i+1} .page${currentPage} p`));
+				}
+
+				let preDateImgs = [];
+				let preTitles = [];
+				let preIcons = [];
+				let preScheduleTexts = [];
+
+				for(let i = 0 ; i < 3 ; i ++){
+					preDateImgs.push(document.querySelector(`#program .day-block${i+1} .page${currentPage-1} .date`));
+					preTitles.push(document.querySelector(`#program .day-block${i+1} .page${currentPage-1} .title`));
+					preIcons.push(document.querySelector(`#program .day-block${i+1} .page${currentPage-1} .icon`));
+					preScheduleTexts.push(document.querySelectorAll(`#program .day-block${i+1} .page${currentPage-1} p`));
+				}
+
+				for(let i = 0 ; i < 3 ; i ++){
+
+					let dateImgsTimeLine = new TimelineMax();
+					let titlesTimeLine = new TimelineMax();
+					let iconsTimeLine = new TimelineMax();
+					let scheduleTextsTimeLine = new TimelineMax();
+
+					dateImgsTimeLine
+						.fromTo(preDateImgs[i],0.5,{opacity:1},{opacity:0})
+						.fromTo(currentDateImgs[i],0.5,{opacity:0},{opacity:1});
+					titlesTimeLine
+						.fromTo(preTitles[i], 0.5,{opacity:1,x:0},{ opacity:0,x:-30,ease:Power4.easeIn})
+				   		.fromTo(currentTitles[i], 0.5, {opacity:0, x:30}, {opacity:1,x:0,ease: Power4.easeIn});
+					iconsTimeLine
+						.fromTo(preIcons[i], 0.5,{opacity:1,x:0},{ opacity:0,x:-30,ease:Power4.easeIn})
+				   		.fromTo(currentIcons[i], 0.5, {opacity:0, x:30}, {opacity:1,x:0,ease: Power4.easeIn, onComplete:enableBtnHandler});
+					scheduleTextsTimeLine
+						.staggerFromTo(preScheduleTexts[i], 0.5,{opacity:1,y:0},{ opacity:0,y:-30,ease:Power4.easeIn},0.03)
+				   		.staggerFromTo(currentScheduleTexts[i], 0.5, {opacity:0, y:30}, {opacity:1,y:0,ease: Power4.easeIn},0.03);
+				}
+				
+				
+			} else {
+				currentPage --;
+			}	
+			
+		}
+
+		backBtn.addEventListener("click",backBtnHandler);
+		nextBtn.addEventListener("click",nextBtnHandler);
 
 	});
 })();
