89b5d23c6d3f6d28af7ad2a3215bd1a9a8e750a6
ktyang86267 <aroe86267@gmail.com>
2017-06-02 12:07:54 +0800

    updatAudio

--- js/main.js
+++ js/main.js
@@ -14,19 +14,6 @@ $(document).ready(function(){
     });
 });
 
-$(document).ready(function(){
-  // scroll to element
-    $('.container a[href^="#"]').on('click',function (e) {
-      e.preventDefault();
-
-      var target = this.hash;
-      var $target = $(target);
-
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top
-      }, 900, 'swing');
-    });
-});
 
 $(function(){
   var audio = document.getElementById('background-audio');
